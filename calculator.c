0ecdfbce76d3f5416940c88ed552fb8bf9834924
mattlourenco27 <matt.lourenco27@gmail.com>
2019-06-25 09:56:06 -0400

    User input is functional

--- calculator.c
+++ calculator.c
@@ -5,3 +5,25 @@
  *
  */
 
+#include <stdio.h>
+#include <string.h>
+#include "quadf.h"
+
+//This function displays a message then recieves the response
+void *getInput(char *message, char *response) {
+	printf(message);
+	gets(response);
+}
+
+int main(int argc, char **argv) {
+	unsigned int MAX_INPUT_CHARACTERS = 256;
+	char input[MAX_INPUT_CHARACTERS + 1];
+
+	//Switch stout to unbuffered mode to fix scan before inputs
+	setvbuf(stdout, 0, _IONBF, 0);
+
+	getInput(">> ", input);
+
+	printf("%s", input);
+
+}
